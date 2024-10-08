c2a4cdb033c5059dc67b9fc59fe013761f57d3d9
Prasad <saranyasasi240@gmail.com>
2024-10-08 12:52:09 +0530

    Added Bean life cycle methods

---
+++ src/main/java/com/logicaswiss/sfg_di/services/CustomBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package com.logicaswiss.sfg_di.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+//        System.out.println("postProcessBeforeInitialization - invoked: " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+//        System.out.println("postProcessAfterInitialization - invoked: " + beanName);
+        return bean;
+    }
+}
---
+++ src/main/java/com/logicaswiss/sfg_di/services/LifeCycleDemoBean.java
@@ -0,0 +1,59 @@
+package com.logicaswiss.sfg_di.services;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware , BeanFactoryAware, ApplicationContextAware {
+    public LifeCycleDemoBean() {
+        System.out.println("I'm in the LifeCycleBean Constructor");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("After Properties Set");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Destroyed");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Set bean name: " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Set bean factory: ");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Set application context");
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("The Post Construct annotated method has been called");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("pre-destroy annotated method has been called");
+    }
+
+    public void beforeInit(){
+        System.out.println("Before Init - Called by Bean Post Processor");
+    }
+
+    public void afterInit(){
+        System.out.println("After init - called by Bean Post Processor");
+    }
+}
