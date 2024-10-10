4809c9e850d3d0b6ea00d1ed5583dc0833c2cdfe
Prasad <saranyasasi240@gmail.com>
2024-10-10 18:55:22 +0530

    Optimized the switch statement

--- src/main/java/com/logicaswiss/pets/PetServiceFactory.java
+++ src/main/java/com/logicaswiss/pets/PetServiceFactory.java
@@ -3,9 +3,6 @@ package com.logicaswiss.pets;
 public class PetServiceFactory {
     public PetService getPetService(String petType) {
         return switch (petType) {
-            case "dog" -> {
-                yield new DogPetService();
-            }
             case "cat" -> {
                 yield new CatPetService();
             }
